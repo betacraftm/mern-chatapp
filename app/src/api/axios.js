89419ee7db667e37687b146c74286f85b24f6084
Betacraft <hoangkimc9@gmail.com>
2024-05-05 21:25:41 +0700

    Change base url in axios.js

--- app/src/api/axios.js
+++ app/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8904";
+const BASE_URL = "https://mern-chatapp-rr9s.onrender.com";
 
 export default axios.create({
   baseURL: BASE_URL,
